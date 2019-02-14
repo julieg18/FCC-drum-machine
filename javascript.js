8e2baeadf25a4a03591aa1993251fc45a362cdc8
julieg18 <julieheartcrochet@gmail.com>
2019-02-14 06:40:27 -0600

    Added jQuery functions and edited print function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,51 @@
 
 
+$(document).ready(function() {
+  $("h1").css("color", "yellow");
+  $(document).keydown( function(pressedKey) {
+    let keyCode = pressedKey.keyCode;
+    switch(keyCode) {
+      case (81):
+      playAudio("qKey");
+      print("qKey");
+      break;
+      case (87):
+      playAudio("wKey");
+      print("wKey");
+      break;
+      case (69):
+      playAudio("eKey");
+      print("eKey");
+      break;
+      case (65):
+      playAudio("aKey");
+      print("aKey");
+      break;
+      case (83):
+      playAudio("sKey");
+      print("sKey");
+      break;
+      case (68):
+      playAudio("dKey");
+      print("dKey");
+      break;
+      case (90):
+      playAudio("zKey");
+      print("zKey");
+      break;
+      case (88):
+      playAudio("xKey");
+      print("xKey");
+      break;
+      case (67):
+      playAudio("cKey");
+      print("cKey");
+      break;
+    }
+  });
+  });
+
+
 function playAudio(key) {
   switch(key) {
     case "qKey":
@@ -34,30 +80,31 @@ function playAudio(key) {
 function print(key) {
   switch(key) {
     case "qKey":
-      document.getElementById("keyString").innerHTML = "I'm a q Key.";
+      document.getElementById("keyString").innerHTML = "Heater 1";
       break;
     case "wKey":
-      document.getElementById("keyString").innerHTML = "I'm a w key."
+      document.getElementById("keyString").innerHTML = "Heater 2"
       break;
     case "eKey":
-      document.getElementById("keyString").innerHTML = "I'm a e key."
+      document.getElementById("keyString").innerHTML = "Heater 3."
       break;
     case "aKey":
-      document.getElementById("keyString").innerHTML = "I'm a a key."
+      document.getElementById("keyString").innerHTML = "Heater 4"
       break;
     case "sKey":
-      document.getElementById("keyString").innerHTML = "I'm a s key."
+      document.getElementById("keyString").innerHTML = "Clap"
       break;
     case "dKey":
-      document.getElementById("keyString").innerHTML = "I'm a d key."
+      document.getElementById("keyString").innerHTML = "Open HH"
       break;
     case "zKey":
-      document.getElementById("keyString").innerHTML = "I'm a z key."
+      document.getElementById("keyString").innerHTML = "Kick n' Hat"
       break;
     case "xKey":
-      document.getElementById("keyString").innerHTML = "I'm a x key."
+      document.getElementById("keyString").innerHTML = "Kick"
       break;
     case "cKey":
-        document.getElementById("keyString").innerHTML = "I'm a c key."
+        document.getElementById("keyString").innerHTML = "Closed HH"
       }
 }
+
