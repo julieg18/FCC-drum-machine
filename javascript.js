8e630cc2ecf0128e85c8471e69028c9072cd28b1
julieg18 <julieheartcrochet@gmail.com>
2019-05-18 11:00:10 -0500

    simplified functions

--- javascript.js
+++ javascript.js
@@ -1,109 +1,52 @@
-
-
 $(document).ready(function() {
   $(document).keydown( function(pressedKey) {
     let keyCode = pressedKey.keyCode;
     switch(keyCode) {
       case (81):
-      playAudio("qKey");
-      print("qKey");
+      playAudio("Q");
+      printPhrase("Heater 1");
       break;
       case (87):
-      playAudio("wKey");
-      print("wKey");
+      playAudio("W");
+      printPhrase("Heater 2");
       break;
       case (69):
-      playAudio("eKey");
-      print("eKey");
+      playAudio("E");
+      printPhrase("Heater 3");
       break;
       case (65):
-      playAudio("aKey");
-      print("aKey");
+      playAudio("A");
+      printPhrase("Heater 4");
       break;
       case (83):
-      playAudio("sKey");
-      print("sKey");
+      playAudio("S");
+      printPhrase("Clap");
       break;
       case (68):
-      playAudio("dKey");
-      print("dKey");
+      playAudio("D");
+      printPhrase("Open HH");
       break;
       case (90):
-      playAudio("zKey");
-      print("zKey");
+      playAudio("Z");
+      printPhrase("Kick n' Hat");
       break;
       case (88):
-      playAudio("xKey");
-      print("xKey");
+      playAudio("X");
+      printPhrase("Kick");
       break;
       case (67):
-      playAudio("cKey");
-      print("cKey");
+      playAudio("C");
+      printPhrase("Closed HH");
       break;
     }
   });
-  });
+});
 
 
 function playAudio(key) {
-  switch(key) {
-    case "qKey":
-      document.getElementById("Q").play();
-      break;
-    case "wKey":
-      document.getElementById("W").play();
-      break;
-    case "eKey":
-      document.getElementById("E").play();
-      break;
-    case "aKey":
-      document.getElementById("A").play();
-      break;
-    case "sKey":
-      document.getElementById("S").play();
-      break;
-    case "dKey":
-      document.getElementById("D").play();
-      break;
-    case "zKey":
-      document.getElementById("Z").play();
-      break;
-    case "xKey":
-      document.getElementById("X").play();
-      break;
-    case "cKey":
-      document.getElementById("C").play();
-  }
+  $(`#${key}`)[0].play()
 }
 
-function print(key) {
-  switch(key) {
-    case "qKey":
-      document.getElementById("keyString").innerHTML = "Heater 1";
-      break;
-    case "wKey":
-      document.getElementById("keyString").innerHTML = "Heater 2"
-      break;
-    case "eKey":
-      document.getElementById("keyString").innerHTML = "Heater 3"
-      break;
-    case "aKey":
-      document.getElementById("keyString").innerHTML = "Heater 4"
-      break;
-    case "sKey":
-      document.getElementById("keyString").innerHTML = "Clap"
-      break;
-    case "dKey":
-      document.getElementById("keyString").innerHTML = "Open HH"
-      break;
-    case "zKey":
-      document.getElementById("keyString").innerHTML = "Kick n' Hat"
-      break;
-    case "xKey":
-      document.getElementById("keyString").innerHTML = "Kick"
-      break;
-    case "cKey":
-        document.getElementById("keyString").innerHTML = "Closed HH"
-      }
-}
-
+function printPhrase(phrase) {
+  $('#keyString').html(`<p>${phrase}</p>`)
+} 
